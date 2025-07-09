3c2a11b4bf8d11b5e89a2e9a9aa906112c20a2d7
Suhendra Mandala Ernas <mandalasuhendra@gmail.com>
2025-07-09 15:18:00 +0700

    Update belajar_dengue_app.py

--- belajar_dengue_app.py
+++ belajar_dengue_app.py
@@ -30,9 +30,7 @@ with st.form("input_form"):
     Trombosit = st.number_input("Trombosit (10³/uL)", min_value=5.0, max_value=500.0, value=150.0)
     Neutrofil = st.number_input("Neutrofil (%)", min_value=0.0, max_value=100.0, value=55.0)
     Limfosit = st.number_input("Limfosit (%)", min_value=0.0, max_value=100.0, value=35.0)
-    MPV = st.number_input("MPV (fL)", min_value=5.0, max_value=15.0, value=9.0)
-    PDW = st.number_input("PDW", min_value=5.0, max_value=30.0, value=14.0)
-    MCV = st.number_input("MCV (fL)", min_value=60.0, max_value=120.0, value=85.0)
+    MCV = st.number_input("MCV (fL)", min_value=60.0, max_value=120.0, value=850)
     MCH = st.number_input("MCH (pg)", min_value=15.0, max_value=45.0, value=28.0)
     MCHC = st.number_input("MCHC (%)", min_value=20.0, max_value=40.0, value=33.0)
 
@@ -47,8 +45,6 @@ if submitted:
         'Trombosit_10^3_uL': [Trombosit],
         'Neutrofil_percent': [Neutrofil],
         'Limfosit_percent': [Limfosit],
-        'MPV_fL': [MPV],
-        'PDW': [PDW],
         'MCV_fL': [MCV],
         'MCH_pg': [MCH],
         'MCHC_percent': [MCHC]
